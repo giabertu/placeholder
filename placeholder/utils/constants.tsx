672c078368966935b8cf6de221fc9f91bdaa4aab
giabertu <giabertu99@gmail.com>
2022-08-16 17:17:24 +0100

    refactor: made mentortalk component out of page2

--- placeholder/utils/constants.tsx
+++ placeholder/utils/constants.tsx
@@ -1,3 +1,13 @@
-export const choicesArray = ['learning how to program', 'switching careers', 'finding my dream developer role', 'writing better code'];
+export const beginnerChoices = ['learning how to program', 'switching careers', 'finding my dream developer role', 'writing better code'];
 
 export const roles = ['frontend', 'backend', 'full stack', 'mobile', 'game', 'data scientist', 'devops', 'web3']
+
+export const beginnerDescriptions = [
+  'Programming can be intimidating, and it can be difficult to know where to start. I\'d like a mentor that can show me the ropes and give me some direction, or teach me about a specific technology.',
+
+  'I am considering becoming a Software Developer and I would like to ask someone in the field more about it. I am looking for insights and practical advice to help me make a potential switch.',
+
+  'A career in developing is highly rewarding and challenging, but it may be a daunting prospect for some.I\'d like a mentor that can takl authenticly about the good and the bad of different roles.',
+
+  'I have been coding for some time and I am ready to take it up a notch. I am looking for someone that will review my code and suggest improvements that will improve my skill and help me code more confidently.',
+]
