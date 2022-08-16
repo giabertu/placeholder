5d13c09c57171396fec4992e9f143ed9d1fd3788
Sau Ching Loke <loke.sau.ching@gmail.com>
2022-08-16 10:19:12 +0100

    style: buttons animation

--- placeholder/pages/_app.tsx
+++ placeholder/pages/_app.tsx
@@ -1,5 +1,4 @@
-import '../styles/globals.css'
-import '../styles/page1.css'
+import '../styles/globals.css';
 import type { AppProps } from 'next/app'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
--- placeholder/pages/quiz_init/page1.tsx
+++ placeholder/pages/quiz_init/page1.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import ProgressBar from '../../components/ProgressBar';
 import QuizCompanyName from '../../components/QuizCompanyName';
+import styles from '../../styles/page1.module.css'
 
 
 
@@ -16,44 +17,44 @@ export default function Page1() {
 
 
   return (
-    <div className='page1-container'>
+    <div className={styles.container}>
       <QuizCompanyName />
-      <div className='question-container'>
+      <div className={styles.questionContainer}>
         <ProgressBar value={10} />
-        <div className='title'>
+        <div className={styles.title}>
           {level === '' &&
             <h1> > I am a ...... developer</h1>
           }
           {level === 'beginner' && 
-            <h1> > I am a <span className='level'> {level}</span> developer.</h1>
+            <h1> > I am a <span className={styles.level}> {level}</span> developer.</h1>
           }
 
-          {level === `senior` &&
-            <h1> > I am a <span className='level'> {level}</span> developer. </h1>
+          {level === `advanced` &&
+            <h1> > I am an <span className={styles.level}> {level}</span> developer. </h1>
           } 
 
           {level === 'intermediate' &&
-            <h1> > I am an <span className='level'> {level}</span> developer.</h1>
+            <h1> > I am an <span className={styles.level}> {level}</span> developer.</h1>
           }
         </div>
         
-        <div className='options'>
-          <button className='level-btn' value='beginner' onClick={handleLevel}> {`>`} beginner </button>
-          <button className='level-btn' value='intermediate' onClick={handleLevel}> {`>`} intermediate</button>
-          <button className='level-btn' value='senior' onClick={handleLevel}> {`>`} senior</button>
+        <div className={styles.options}>
+          <button className={styles.btn} value='beginner' onClick={handleLevel}> {`>`} beginner </button>
+          <button className={styles.btn} value='intermediate' onClick={handleLevel}> {`>`} intermediate</button>
+          <button className={styles.btn} value='advanced' onClick={handleLevel}> {`>`} advanced</button>
         </div>
 
-        <div className='description-container'>
+        <div className={styles.descriptionContainer}>
           
           {level === 'beginner' && 
-            <h1 className='description'>A beginner developer would have bla bla bla qualities.</h1>
+            <h1 className={styles.description}>A beginner developer would have bla bla bla qualities.</h1>
           }
           {level === 'intermediate' &&
-            <h1 className='description'>An intermediate developer would have bla bla bla qualities.</h1>
+            <h1 className={styles.description}>An intermediate developer would have bla bla bla qualities.</h1>
           }
 
-          {level === 'senior' &&
-            <h1 className='description'>A senior developer would have bla bla bla qualities.</h1>
+          {level === 'advanced' &&
+            <h1 className={styles.description}>An advanced developer would have bla bla bla qualities.</h1>
           }
 
 
