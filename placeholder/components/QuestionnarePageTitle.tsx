95cd22b0d5d356b568beb9c1d31788ab0d7602bf
Kiavash <kiavash89@gmail.com>
2022-08-17 15:31:26 +0100

    feat: added new technology choices

---
+++ placeholder/components/QuestionnarePageTitle.tsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+// function QuestionnarePageTitle({title}) {
+//   return (
+//     <div>QuestionnarePageTitle</div>
+//   )
+// }
+
+// export default QuestionnarePageTitle
