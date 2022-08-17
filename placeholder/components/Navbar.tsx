54ff209bbddcdec4eae85b0525329bb73f4555f7
Sau Ching Loke <loke.sau.ching@gmail.com>
2022-08-17 19:32:33 +0100

    refactor: dark mode styling

--- placeholder/components/Navbar.tsx
+++ placeholder/components/Navbar.tsx
@@ -1,11 +1,15 @@
 import ColorModeToggle from "./ColorModeToggle";
 import QuizCompanyName from "./QuizCompanyName";
 import styles from '../styles/Navbar.module.css';
+import { useColorMode } from "@chakra-ui/react";
 
 
 export default function Navbar() {
+  const {colorMode} = useColorMode();
+  const isDark = colorMode === 'dark';
+
   return (
-    <div className={styles.container}>
+    <div className={isDark ? styles.containerDarkMode : styles.container}>
       <QuizCompanyName />
       <ColorModeToggle />
     </div>
