79a4af0e3f543e8bd0d6185daad69801f66e4def
giabertu <giabertu99@gmail.com>
2022-08-20 10:00:20 +0100

    fix: roles issue

--- placeholder/pages/quiz_init/beginner/which_careers.tsx
+++ placeholder/pages/quiz_init/beginner/which_careers.tsx
@@ -4,17 +4,17 @@ import Navbar from "../../../components/Navbar";
 import uniqid from "uniqid";
 import ProgressBar from "../../../components/ProgressBar";
 import QuizNavigationButtons from "../../../components/QuizNavigationButtons";
-import { roles } from '../../../utils/constants'
+import { rolesChoices } from '../../../utils/constants'
 import styles from '../../../styles/which_careers.module.css'
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
-import {beginnerChangedesiredCareers} from '../../../redux/slices/mentorPreferencesSlice'
+import { beginnerChangedesiredCareers } from '../../../redux/slices/mentorPreferencesSlice'
 import QuestionnaireButton from "../../../components/QuestionnaireButton";
 
 function WhichCareers() {
 
   const dispatch = useAppDispatch();
   const careers = useAppSelector((state) => state.beginnerForm.desiredCareers);
-  const {colorMode} = useColorMode();
+  const { colorMode } = useColorMode();
   const isDark = colorMode === 'dark';
 
   const handleCareers = function (event: React.MouseEvent<HTMLButtonElement>) {
@@ -30,34 +30,32 @@ function WhichCareers() {
 
   return (
     <div className={styles.container}>
-      <Navbar/>
+      <Navbar />
       <div className={styles.formContainer}>
-        <ProgressBar value={20}/>
+        <ProgressBar value={20} />
         <div className={styles.title}>
           {!careers.length ? <h1 className={styles.title}>&#62; Roles I'd be interested in: <span className={styles.underline}>___</span></h1>
-          : careers[0] === "I'm unsure at this stage" ? <h1 className={styles.title}>&#62; Roles I'd be interested in: ["I'm unsure at this stage"]</h1> :
-          <h1 className={styles.title}>&#62; Roles I'd be interested in: <span className='h1-span'>{getStringifiedArray()}</span></h1>}
+            : careers[0] === "I'm unsure at this stage" ? <h1 className={styles.title}>&#62; Roles I'd be interested in: ["I'm unsure at this stage"]</h1> :
+              <h1 className={styles.title}>&#62; Roles I'd be interested in: <span className='h1-span'>{getStringifiedArray()}</span></h1>}
         </div>
 
         <div className={styles.options}>
-          {roles.map((role: string) =>
+          {rolesChoices.map((role: string) =>
 
             <button className={isDark ? styles.btnDarkMode : styles.btn} key={uniqid()} value={role} onClick={handleCareers}>
               &gt; {role}
 
-
             </button>)}
-            <button className={isDark ? styles.btnDarkMode : styles.btn} value="general" onClick={handleCareers}> &gt; I'm unsure at this stage</button>
+          <button className={isDark ? styles.btnDarkMode : styles.btn} value="general" onClick={handleCareers}> &gt; I'm unsure at this stage</button>
         </div>
         {/* <QuestionnaireButton text="I'm unsure at this stage" value="general" onClick={handleCareers}/> */}
-
 
       </div>
 
       <div className={styles.navigationBtns}>
-          <QuizNavigationButtons back='/quiz_init/beginner/which_technologies' next="quiz_init/page10"/>
+        <QuizNavigationButtons back='/quiz_init/beginner/which_technologies' next="quiz_init/page10" />
 
-        </div>
+      </div>
     </div>
   )
 
