334600cca80b2992411d3f8b8ef5482b0d366407
giabertu <giabertu99@gmail.com>
2022-08-22 13:53:19 +0100

    refactor: changed user schema

--- placeholder/lib/models/User.ts
+++ placeholder/lib/models/User.ts
@@ -3,54 +3,62 @@ import { TechnologyObj } from '../../redux/types'
 
 //Define new and updated schema
 const userSchema = {
-  name: String,
+  username: String,
   email:  String, 
-  avatar: String, 
-  bio: String, 
-  location: String,
-  level: String,
-  purpose: String,
-  developerField: String,
-  mentorPreferences: {
-    desiredCareers: [String],
-    desiredCategories: [String],
-    desiredTechnologies: [{
-      name: String,
-      imageSrc: String
-    }]
-  },
-  menteePreferences: {
-    desiredCategories: [String],
-    desiredTechnologies: [{
-      name: String,
-      imageSrc: String,
-    }]
-  },
-  experiencedWithTechnologies: [{
-    name : String,
-    imageSrc : String
-    }]
+  first_name: String,
+  last_name: String,
+  custom_json: {
+    avatar: String, 
+    bio: String, 
+    location: String,
+    level: String,
+    purpose: String,
+    developerField: String,
+    mentorPreferences: {
+      desiredCareers: [String],
+      desiredCategories: [String],
+      desiredTechnologies: [{
+        name: String,
+        imageSrc: String
+      }]
+    },
+    menteePreferences: {
+      desiredCategories: [String],
+      desiredTechnologies: [{
+        name: String,
+        imageSrc: String,
+      }]
+    },
+    experiencedWithTechnologies: [{
+      name : String,
+      imageSrc : String
+      }]
+  }
 }
 
 export interface UserType {
-  name: string,
+  username: string,
   email: string,
-  avatar: string,
-  bio: string,
-  location: string,
-  level: string,
-  purpose: string,
-  developerField: null | string,
-  mentorPreferences: {
-    desiredCareers: string[],
-    desiredCategories: string[],
-    desiredTechnologies: (TechnologyObj | string)[]
-  },
-  menteePreferences: {
-    desiredCategories: string[],
-    desiredTechnologies: TechnologyObj[]
+  first_name: string,
+  last_name: string,
+  custom_json: {
+    avatar: string,
+    bio: string,
+    location: string,
+    level: string,
+    purpose: string,
+    developerField: null | string,
+    mentorPreferences: {
+      desiredCareers: string[],
+      desiredCategories: string[],
+      desiredTechnologies: (TechnologyObj | string)[]
+    },
+    menteePreferences: {
+      desiredCategories: string[],
+      desiredTechnologies: TechnologyObj[]
+    }
+    experiencedWithTechnologies: TechnologyObj[]
   }
-  experiencedWithTechnologies: TechnologyObj[]
 }
 
 export const User = mongoose.model("User", new mongoose.Schema(userSchema), "users")
