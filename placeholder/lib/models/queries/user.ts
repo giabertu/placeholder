8a127c17a8a7286a559ffc38aa233b25a7350a15
giabertu <giabertu99@gmail.com>
2022-08-22 14:19:53 +0100

    feat: changed schema, now working

--- placeholder/lib/models/queries/user.ts
+++ placeholder/lib/models/queries/user.ts
@@ -2,7 +2,7 @@ import {User, UserType} from '../User'
 
 async function updateUser(user: UserType) {
   console.log('Here is the user in queries' , user)
-  const res = await User.findOneAndUpdate({email: user.email}, user )
+  const res = await User.replaceOne({email: user.email}, user )
   console.log('Here is the result of the update: ', res)
   return res
 }
