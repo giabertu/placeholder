7779224f0430fe18280800b06ff4a1710e5bc0d2
giabertu <giabertu99@gmail.com>
2022-08-24 19:08:20 +0100

    feat: implemented create chat method

--- placeholder/services/ChatEngineApi.ts
+++ placeholder/services/ChatEngineApi.ts
@@ -4,13 +4,13 @@ import { UserType } from "../lib/models/User"
 
 class ChatEngineApi {
 
-  static USER_URL = 'https://api.chatengine.io/users/'
+  static CHAT_ENGINE_URL = 'https://api.chatengine.io/'
 
 
   static async createUser (user: UserType) {
     console.log('User to be sent to chat engine: ', JSON.stringify(user))
 
-    const res = await fetch(this.USER_URL, {
+    const res = await fetch(`${this.CHAT_ENGINE_URL}users/`, {
       method: "POST",
       headers: {
         'Content-Type': 'application/json',
@@ -28,18 +28,41 @@ class ChatEngineApi {
   }
 
   static async getChatEngineUser({username, secret}: {username: string, secret: string}) {
-    const res = await fetch(this.USER_URL, {
+    try {
+
+      const res = await fetch(`${this.CHAT_ENGINE_URL}users/`, {
+        method: 'PUT',
+        headers : {
+          'Content-type': 'application/json',
+          'PRIVATE-KEY' : 'e3c4f715-cacd-4a8c-a00a-038a9849edff'
+        },
+        body: JSON.stringify({username, secret})
+      })
+      console.log('Response: ', res)
+      return await res.json()
+    } catch (error) {
+      console.log('ChatEngine fetch user error 🔴', error)
+    }
+    return false;
+  }
+
+  static async getOrCreateChat({ownUsername, ownSecret, usernameToChat}: {ownUsername: string, ownSecret: string, usernameToChat: string}) {
+    const res = await fetch(`${this.CHAT_ENGINE_URL}chats/`, {
       method: 'PUT',
       headers : {
         'Content-type': 'application/json',
-        'PRIVATE-KEY' : 'e3c4f715-cacd-4a8c-a00a-038a9849edff'
+        'Project-ID' : 'd6620cc4-d139-4ed9-85f7-cea40cd73c40',
+        'User-Name' : ownUsername,
+        'User-Secret' : ownSecret,
       },
-      body: JSON.stringify({username, secret})
+      body: JSON.stringify({
+        usernames: [usernameToChat],
+        title: 'My Chat',
+        is_direct_chat: true
+      }) 
     })
-    console.log('Response: ', res)
-    return await res.json()
+    return await res.json();
   }
-
 }
 
 export default ChatEngineApi;
