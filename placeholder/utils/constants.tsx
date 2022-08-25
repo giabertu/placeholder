69b293b9c4fe82220456b7f38cb16163b26129d0
Kiavash <kiavash89@gmail.com>
2022-08-25 08:49:01 +0100

    feat: remove development buttons from homepage, tweak styles

--- placeholder/utils/constants.tsx
+++ placeholder/utils/constants.tsx
@@ -18,11 +18,11 @@ export const levelChoices = [
 ]
 
 export const levelDescriptions = [
-  "I am just starting out as a software developer and aiming to learn basic programming foundations. / I have been consistently learning how to program and I want to prepare for the next stage to land my first software developing role.",
+  "I'm just starting out as a software developer and aiming to learn basic programming foundations. / I have been consistently learning how to program and I want to prepare for the next stage to land my first software developing role.",
 
-  "I am a software developer with at least 2 years of experience in the industry and I have 1 or more language of mastery.",
+  "I'm a software developer with at least 2 years of professional / personal experience, and a strong portfolio. I feel comfortable teaching programming concepts to those who are learning the ropes.",
 
-  'I am a software developer with at least 5 years of experience in the industry. I have past experience in leading junior-mid level developers/working independently.'
+  'I\'m a software developer with at least 5 years of experience in the industry. I have experience both working independently and leading junior-mid level developers independently.'
 ]
 
 export const int_advChoices = [
@@ -32,7 +32,7 @@ export const int_advChoices = [
 export const int_advDescriptions = [
   'I already have experience and I am looking for someone to guide me in learning a new language, framework or library. Or even talk about programming concepts.',
 
-  'I am interested in taking my career to the next step or in switching to a different role in which I have less experience. I would like someone to give me interview tips, review my CV and give me advice.',
+  'I\'m interested in taking my career to the next step or in switching to a different role in which I have less experience. I\'d like someone to give me interview tips, review my CV, or just give me general advice.',
 
   'I know what I am doing but I would like to improve my coding skills. I would like to have someone more experienced review my code and suggest refactors.',
 ]
